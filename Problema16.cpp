1b5a2946fd516583b39af0bb0a4a373a87655b40
XxPabonxX <diego.pabon@udea.edu.co>
2023-03-27 09:41:09 -0500

    Programa completo

--- Problema16.cpp
+++ Problema16.cpp
@@ -10,10 +10,24 @@ Problema 16.
 
 using namespace std;
 
-int Problema16(void){
+int Problema16(){
+
+    int tam, F=1, Z=1;
+
+    cout << "Ingrese la dimencion de la matriz: ";
+    cin >> tam;
+
+    for (int i = tam; i > 1; i--){
+        Z = Z*i;
+    }
 
 
+    for (int i = tam + tam; i > 1; i--){
+        F = F*i;
+    }
+
+    F = F / (Z * Z);
+    cout << "Para una malla de: " << tam << "x" << tam << " Hay " << F << " Caminos" << endl;
 
     return 0;
-
 }
--- Problema18.cpp
+++ Problema18.cpp
@@ -10,10 +10,76 @@ Problema 18.
 
 using namespace std;
 
-int Problema18(void){
+int factorial(int);
+void nPermutacion(int);
 
+int Problema18() {
 
+    int n = 0;
+
+    cout << "Ingrese el enesimo termino: ";
+    cin >> n;
+
+    nPermutacion(n);
 
     return 0;
+
+}
+
+
+// Función para calcular el factorial de un número
+
+int factorial(int n){
+
+    int fact=1;
+
+    for (int i=1; i <=n ; i++){
+
+        fact*=i;
+
+    }
+
+    return fact;
+
+}
+
+void nPermutacion(int n){
+
+    // Generar un arreglo de números del 0 al 9
+    int semilla = n, fact = 0, posicionactual, temp = 0;
+    int nums[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    // Calcular la permutación lexicográfica correspondiente
+
+    for (int i = 0; i < 10; i++){
+
+        fact = factorial(9-i);
+        posicionactual=i+n/fact;
+        n%=fact;
+
+        while (posicionactual>i){
+
+            temp = nums[posicionactual];
+            nums[posicionactual] = nums[posicionactual-1];
+            nums[posicionactual-1] = temp;
+            posicionactual--;
+
+        }
+
+        if (n == 0) {
+
+            break;
+
+        }
+    }
+
+    cout << "La permutacion numero: " << semilla << " es: ";
+    for (int i=0; i<9; i++) {
+
+        cout << nums[i];
+
+    }
+
+    cout << endl;
 
 }
